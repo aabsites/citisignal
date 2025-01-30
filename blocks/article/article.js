97706a57a5a7fd0e2eb39e60b873b18fd8ad1d9b
timwlee <timothyl@adobe.com>
2025-01-30 10:12:38 -0500

    Overwrite repo

--- blocks/article/article.js
+++
@@ -1,9 +0,0 @@
-/**
- * loads and decorates the article block
- * @param {Element} block The article block element
- */
-
-export default async function decorate(block) {
-  const firstChild = block.children[0];
-  firstChild.className = 'article-content-wrapper';
-}
